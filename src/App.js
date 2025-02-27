3a782d2f74befcb6f3aa4f608619102dda61570f
Andrea Calligari <153660323+Andrea-Calligari@users.noreply.github.com>
2025-02-27 16:51:03 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AppRouter from './router/AppRouter';
+import './assets/styles/App.css';
+import './assets/styles/input.css';
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <AppRouter />
+    </div>
+
+  );
+}
+
+export default App;
